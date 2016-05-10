b0b2c92b361ed8f44c8ade5073a81a3530602aab
sonvisal <sonvisal10@gmail.com>
2016-05-10 14:35:40 +0700

    [buge] change file run.sh

--- run.sh
+++ run.sh
@@ -12,9 +12,8 @@ cat title_product.txt | while read line
             When I am  go page detail '""'"'$line'"
             Then I should page detail
             When I can check image url
-            Then I can see image found or not\n' &>>product.feature
+            Then I can see image found or not\n' &>>features/product.feature
 
     done
-    mv product.feature /home/visal/Desktop/test_safir_404/features/
     chimp --ddp=http://localhost:3000 --watch
 
